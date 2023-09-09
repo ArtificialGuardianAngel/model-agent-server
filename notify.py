617e9b2f814cdb6deb4f74894de86393827bf4f2
Максим Касакин <loliallen@MacBook-Air-Maksim.local>
2023-09-09 16:54:50 +0300

    fixed issue with empy onject

--- notify.py
+++ notify.py
@@ -3,5 +3,4 @@ import json
 
 
 def notify(data):
-    json_data = json.dumps(data)
-    requests.post('http://localhost:4444/slack', data=json_data)
+    requests.post('http://localhost:4444/slack', data=data)
